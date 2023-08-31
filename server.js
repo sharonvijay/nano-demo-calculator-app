f6c8be79523f85b397053a906a2ba9a88a420cee
Sharon Vijay <94788632+sharonvijay@users.noreply.github.com>
2023-08-31 12:21:10 +0000

    31-Aug-23

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 const baseRouter = express.Router();
 
 baseRouter.get('/greeting', (req, res) => {
-    return res.send('');
+    return res.send('Hello World!').status(200);
 });
 
 baseRouter.post('/add', (req, res) => {
